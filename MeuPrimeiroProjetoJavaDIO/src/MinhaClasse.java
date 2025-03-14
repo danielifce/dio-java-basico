04d740d521d0642436e9481653410c616d59f7e3
danielifce <danielderbox@gmail.com>
2025-03-14 14:48:19 -0300

    Não lembro

--- MeuPrimeiroProjetoJavaDIO/src/MinhaClasse.java
+++ MeuPrimeiroProjetoJavaDIO/src/MinhaClasse.java
@@ -1,9 +1,9 @@
 import javax.print.DocFlavor.STRING;
 
-public class MinhaClasse {
+public class MinhaClasse { // Sempre CamaelCase com inicial maiuscula
     public static void main(String[] args){
-        int variavel_sempre_minuscula = 20;
-        String nome = "João";
+        int variavel_sempre_minuscula = 20; // Variável é sempre minúscula e Se toda Upper Case é uma constanate.
+        String nome = "João"; // Os simbolos permitidos são letras, números, $ e _. Os números só não podem ser o primeiro caractere.
         String sobrenome = "Silva";
         String nomeCompleto = nomeCompleto(nome, sobrenome);
         System.out.println(nomeCompleto); //Retorna o nome completo
