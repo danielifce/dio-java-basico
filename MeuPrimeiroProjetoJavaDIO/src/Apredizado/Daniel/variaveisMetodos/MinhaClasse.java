143be2245e97838a4a17becb27ae9f44fc0f03c2
danielifce <danielderbox@gmail.com>
2025-03-14 18:08:44 -0300

    salvando

--- MeuPrimeiroProjetoJavaDIO/src/Apredizado/Daniel/variaveisMetodos/MinhaClasse.java
+++ MeuPrimeiroProjetoJavaDIO/src/Apredizado/Daniel/variaveisMetodos/MinhaClasse.java
@@ -5,7 +5,7 @@ public class MinhaClasse { // Sempre CamaelCase com inicial maiuscula
     public static void main(String[] args){
         int variavel_sempre_minuscula = 20; // Variável é sempre minúscula e Se toda Upper Case é uma constanate.
         String nome = "João"; // Os simbolos permitidos são letras, números, $ e _. Os números só não podem ser o primeiro caractere.
-        String sobrenome = "Silva";
+        String sobrenome = "Silva";// variáveis são escritas no singular e no idioma do código. plural é para arrey.
         String nomeCompleto = juntarNome(nome, sobrenome);
         System.out.println(nomeCompleto); //Retorna o nome completo
         resultarBoletim(7); //Retorna a situação do aluno
