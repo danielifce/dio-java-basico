01cf85cbc33563c922c6dc4a094102d9f84c10f8
danielifce <danielderbox@gmail.com>
2025-03-14 15:14:50 -0300

    Salvando

--- MeuPrimeiroProjetoJavaDIO/src/MinhaClasse.java
+++ MeuPrimeiroProjetoJavaDIO/src/MinhaClasse.java
@@ -5,12 +5,21 @@ public class MinhaClasse { // Sempre CamaelCase com inicial maiuscula
         int variavel_sempre_minuscula = 20; // Variável é sempre minúscula e Se toda Upper Case é uma constanate.
         String nome = "João"; // Os simbolos permitidos são letras, números, $ e _. Os números só não podem ser o primeiro caractere.
         String sobrenome = "Silva";
-        String nomeCompleto = nomeCompleto(nome, sobrenome);
+        String nomeCompleto = juntarNome(nome, sobrenome);
         System.out.println(nomeCompleto); //Retorna o nome completo
-
+        resultarBoletim(7); //Retorna a situação do aluno
     }
 
-    public static String nomeCompleto (String nome, String sobrenome){
+    public static String juntarNome (String nome, String sobrenome){ // Métodos tem nome no infinitivo e devem comerça com letra minúscula em CamelCase.
         return nome.concat(" ").concat(sobrenome);
+    }
+    
+    public static void resultarBoletim (int nota){ // Métodos tem nome no infinitivo e devem comerça com letra minúscula em CamelCase.
+        if (nota < 6)
+            System.out.println("Reprovado");
+        else if (nota == 6)
+            System.err.println("Prova de Minerva");
+            else
+                System.out.println("Aprovado");              
     }
 }
