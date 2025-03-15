42c44d32a4fec2c0dd2175869a9792821947bd5f
danielifce <danielderbox@gmail.com>
2025-03-15 19:10:54 -0300

    Controle de fluxo

---
+++ CaixaEletronico/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double saldo = 100;
+        double valorSolicitado = 500.59;
+
+        if(valorSolicitado <= saldo){ // Controle de fluxo
+            saldo = saldo - valorSolicitado;
+            System.out.println("Saque realizado com sucesso!");
+            System.out.println("Saldo atual: " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+}
