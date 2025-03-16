ea9658932f3bbfb22f9d609182dfbe86be0167c9
danielifce <danielderbox@gmail.com>
2025-03-16 10:10:38 -0300

    Comentário formata cep

--- EstruturasDeRepetição/src/FormatadorDeCep.java
+++ EstruturasDeRepetição/src/FormatadorDeCep.java
@@ -1,19 +1,43 @@
-public class FormatadorDeCep {
-
-    public static void main(String[] args) {
-        try{
-        String cepFormatado = formatarCep("00.000-000");
-        }
-        catch(CepInvalidoException e){
-            e.printStackTrace();
-        }
+// Exceção personalizada
+// Criando uma exceção personalizada chamada CepInvalidoException, que herda da classe Exception.
+// Essa exceção será lançada quando o CEP informado não tiver exatamente 8 dígitos.
+class CepInvalidoException extends Exception {
+    // Construtor que recebe uma mensagem de erro
+    public CepInvalidoException(String message) {
+        // Passa a mensagem para a classe pai (Exception)
+        super(message);
     }
 }
 
+public class FormatadorDeCep {
 
-static String formatarCep(String cep) throws CepInvalidoException {
-    if(cep.length() != 8){
-        throw new CepInvalidoException("O CEP deve conter 8 dígitos");
+    public static void main(String[] args) {
+        try {
+            // Testando a formatação do CEP
+            // A função 'formatarCep' é chamada com um CEP válido "12545865".
+            String cepFormatado = formatarCep("12545865");
+            // Imprime o CEP formatado no console
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // Caso uma exceção do tipo CepInvalidoException seja lançada,
+            // este bloco de código irá ser executado.
+            // O método 'printStackTrace' imprime detalhes sobre a exceção no console.
+            e.printStackTrace();
+        }
     }
-    return "00.000-000"//Cep formatado
+
+    // Método estático para formatar o CEP
+    // O método 'formatarCep' recebe um CEP como parâmetro e retorna o CEP formatado ou lança uma exceção
+    static String formatarCep(String cep) throws CepInvalidoException {
+        // Verifica se o CEP tem exatamente 8 dígitos
+        // Caso contrário, uma exceção CepInvalidoException será lançada.
+        if (cep.length() != 8) {
+            // Lançando a exceção personalizada com uma mensagem de erro
+            throw new CepInvalidoException("O CEP deve conter 8 dígitos");
+        }
+        
+        // Se o CEP for válido (tiver 8 dígitos), formata o CEP
+        // Adiciona um hífen (-) entre o 5º e o 6º caractere do CEP
+        return cep.substring(0, 5) + "-" + cep.substring(5);
+    }
 }
