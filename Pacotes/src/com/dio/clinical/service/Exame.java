345c6bbc8b474985b4a752407efeb56f55591268
danielifce <danielderbox@gmail.com>
2025-03-24 09:26:34 -0300

    Clinica a níve de pacotes

--- Pacotes/src/com/dio/clinical/service/Exame.java
+++ Pacotes/src/com/dio/clinical/service/Exame.java
@@ -1,5 +1,10 @@
 package com.dio.clinical.service;
+import com.dio.clinical.model.Cliente;
 
 public class Exame {
+
+    void salvarExame(Cliente cliente) {
+        System.out.println("Exame salvo com sucesso!");
+    }
 
 }
